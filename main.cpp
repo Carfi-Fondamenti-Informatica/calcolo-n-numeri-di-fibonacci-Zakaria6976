5dcd41d6048fedc117117b70a99e61a7cf6bf6fb
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-11-14 13:13:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int n = 0,a=0,b=1,c= a+b;
-    cin>>n;
-    cout << b <<endl;
-    for(int i=2;i < (n+1);i++){
-    if(c<=n) {
-        cout << c<< endl;
-        a=b;
-        b=c;
-        c= a+b;
-    }
-    }
-}
 
+int main() {
+    int n=0, i=0, j=1, k=1, l=1;
+    cin >> n;
+    if (n>1){
+        cout << 1 <<endl;
+        for (l=0;l<n-1;l++){
+            k=i+j;
+            cout << k <<endl;
+            i=j;
+            j=k;
+        }
+    }else{
+        cout << "errore" <<endl;
+    }
+    return 0;
+}
